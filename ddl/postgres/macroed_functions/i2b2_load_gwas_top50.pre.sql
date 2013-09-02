2e1f2d98152976a4779d181ac5c29f10b9c00ec7
Gustavo Lopes <gustavo@thehyve.nl>
2013-09-02 17:03:03 +0200

    ws

--- ddl/postgres/macroed_functions/i2b2_load_gwas_top50.pre.sql
+++ ddl/postgres/macroed_functions/i2b2_load_gwas_top50.pre.sql
@@ -7,12 +7,12 @@ CREATE OR REPLACE FUNCTION tm_cz.i2b2_load_gwas_top50(
 )
 RETURNS BIGINT AS $body$
 DECLARE
-	<?php standard_vars() ?>
+    <?php standard_vars() ?>
 BEGIN
-	<?php func_start('I2B2_LOAD_GWAS_TOP50') ?>
+    <?php func_start('I2B2_LOAD_GWAS_TOP50') ?>
 
     -- Create temporary table tmp_analysis_gwas_top500
-	<?php step_begin() ?>
+    <?php step_begin() ?>
     CREATE TABLE tmp_analysis_gwas_top500 AS
     SELECT
         a.*
@@ -38,22 +38,22 @@ BEGIN
         a
     WHERE
         a.rnum <= 500;
-	<?php step_end('Create temporary table tmp_analysis_gwas_top500') ?>
+    <?php step_end('Create temporary table tmp_analysis_gwas_top500') ?>
 
     -- Create indexes on tmp_analysis_gwas_top500
-	<?php step_begin() ?>
+    <?php step_begin() ?>
     CREATE INDEX CONCURRENTLY t_a_g_t500_idx ON tmp_analysis_gwas_top500(rs_id);
     CREATE INDEX CONCURRENTLY t_a_ga_t500_idx ON tmp_analysis_gwas_top500(bio_assay_analysis_id);
-	<?php step_end('Create indexes on tmp_analysis_gwas_top500', 0) ?>
+    <?php step_end('Create indexes on tmp_analysis_gwas_top500', 0) ?>
 
     -- Drop table biomart.bio_asy_analysis_gwas_top50
-	<?php step_begin() ?>
+    <?php step_begin() ?>
     SELECT COUNT(*) INTO rowCt FROM biomart.bio_asy_analysis_gwas_top50;
     DROP TABLE biomart.bio_asy_analysis_gwas_top50;
-	<?php step_end('Drop table biomart.bio_asy_analysis_gwas_top50', 'rowCt') ?>
+    <?php step_end('Drop table biomart.bio_asy_analysis_gwas_top50', 'rowCt') ?>
 
     -- Recreate table biomart.bio_asy_analysis_gwas_top50
-	<?php step_begin() ?>
+    <?php step_begin() ?>
     CREATE TABLE biomart.bio_asy_analysis_gwas_top50 AS
     SELECT
         baa.bio_assay_analysis_id,
@@ -72,13 +72,13 @@ BEGIN
         JOIN deapp.de_rc_snp_info info ON DATA.rs_id = info.rs_id
             AND ( hg_version = 19 )
         LEFT JOIN deapp.de_snp_gene_map gmap ON gmap.snp_name = info.rs_id;
-	<?php step_end('Recreate table biomart.bio_asy_analysis_gwas_top50') ?>
+    <?php step_end('Recreate table biomart.bio_asy_analysis_gwas_top50') ?>
 
     -- Recreate indexes on biomart.bio_asy_analysis_gwas_top50
-	<?php step_begin() ?>
+    <?php step_begin() ?>
     CREATE INDEX b_asy_gwas_t50_idx1 ON biomart.bio_asy_analysis_gwas_top50(bio_assay_analysis_id) TABLESPACE indx;
     CREATE INDEX b_asy_gwas_t50_idx2 ON biomart.bio_asy_analysis_gwas_top50(analysis) TABLESPACE indx;
-	<?php step_end('Recreate indexes on biomart.bio_asy_analysis_gwas_top50', 0) ?>
+    <?php step_end('Recreate indexes on biomart.bio_asy_analysis_gwas_top50', 0) ?>
 
     <?php func_end() ?>
 END;
