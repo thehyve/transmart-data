3621369f32e2289fc56548acbd0cda825e3929c8
Gustavo Lopes <gustavo@thehyve.nl>
2013-09-03 14:24:09 +0200

    Automatically detect macroed funcs as manual objs

--- ddl/postgres/manual_objects_list.php
+++ ddl/postgres/manual_objects_list.php
@@ -1,15 +1,11 @@
 <?php
 $manual_objects = [
-    'tm_cz' => [
-        ['FUNCTION', 'rwg_add_search_term'],
-        ['FUNCTION', 'rwg_add_taxonomy_term'],
-        ['FUNCTION', 'rwg_create_analysis_entry'],
-        ['FUNCTION', 'rwg_import_from_ext'],
-        ['FUNCTION', 'rwg_load_analysis_data'],
-        ['FUNCTION', 'rwg_load_analysis_metadata'],
-        ['FUNCTION', 'rwg_load_heat_map_results'],
-        ['FUNCTION', 'rwg_remove_study'],
-    ],
+    'tm_cz' => [],
 ];
+
+foreach (glob('macroed_functions/*.pre.sql') as $file) {
+    $manual_objects['tm_cz'][] = ['FUNCTION',
+            preg_replace('/macroed_functions\/(.*)\.pre\.sql/', '$1', $file)];
+}
 
 // vim: et ts=4 sw=4:
