1876d76fbed7f867a984f69370ab3aa5cf919c40
Tim Dorscheidt <tim@thehyve.nl>
2014-07-07 22:16:22 -0400

    Ensured authentication pars are correct

--- samples/oracle/gen_kettle_properties.php
+++ samples/oracle/gen_kettle_properties.php
@@ -1,25 +1,29 @@
 TM_CZ_DB_NAME=<?= $_ENV['ORASID'], "\n" ?>
 TM_CZ_DB_PORT=<?= $_ENV['ORAPORT'], "\n" ?>
-TM_CZ_DB_PWD=tm_cz
+TM_CZ_DB_PWD=TMuser0!
 TM_CZ_DB_SERVER=<?= $_ENV['ORAHOST'], "\n" ?>
 TM_CZ_DB_USER=tm_cz
 TM_LZ_DB_NAME=<?= $_ENV['ORASID'] , "\n" ?>
 TM_LZ_DB_PORT=<?= $_ENV['ORAPORT'] , "\n" ?>
-TM_LZ_DB_PWD=tm_lz
+TM_LZ_DB_PWD=TMuser0!
 TM_LZ_DB_SERVER=<?= $_ENV['ORAHOST'], "\n" ?>
 TM_LZ_DB_USER=tm_lz
 TM_WZ_DB_NAME=<?= $_ENV['ORASID'] , "\n" ?>
 TM_WZ_DB_PORT=<?= $_ENV['ORAPORT'] , "\n" ?>
-TM_WZ_DB_PWD=tm_wz
+TM_WZ_DB_PWD=TMuser0!
 TM_WZ_DB_SERVER=<?= $_ENV['ORAHOST'], "\n" ?>
 TM_WZ_DB_USER=tm_wz
 DEAPP_DB_NAME=<?= $_ENV['ORASID'] , "\n" ?>
 DEAPP_DB_PORT=<?= $_ENV['ORAPORT'] , "\n" ?>
 DEAPP_DB_SERVER=<?= $_ENV['ORAHOST'], "\n" ?>
-DEAPP_DB_PWD=deapp
+DEAPP_PWD=TMuser0!
+DEAPP_USER=deapp
+DEAPP_DB_PWD=TMuser0!
 DEAPP_DB_USER=deapp
 BIOMART_DB_NAME=<?= $_ENV['ORASID'] , "\n" ?>
 BIOMART_DB_PORT=<?= $_ENV['ORAPORT'] , "\n" ?>
 BIOMART_DB_SERVER=<?= $_ENV['ORAHOST'], "\n" ?>
-BIOMART_PWD=biomart
+BIOMART_PWD=TMuser0!
 BIOMART_USER=biomart
+BIOMART_DB_PWD=TMuser0!
+BIOMART_DB_USER=biomart
