a68f5b78fd4ad89c75950b8e64ce3ecdd0de5d21
Jan Hudecek <j.hudecek@nki.nl>
2014-09-30 13:20:09 +0200

    fixed wrong ddl script included

--- ddl/postgres/deapp/_load_all.sql
+++ ddl/postgres/deapp/_load_all.sql
@@ -28,7 +28,10 @@
 \i deapp/de_subject_rnaseq_data.sql
 \i deapp/de_subject_sample_mapping.sql
 \i deapp/de_subject_snp_dataset.sql
-\i deapp/de_two_region_variant.sql
+\i deapp/de_two_region_event.sql
+\i deapp/de_two_region_junction.sql
+\i deapp/de_two_region_event_gene.sql
+\i deapp/de_two_region_junction_event.sql
 \i deapp/de_variant_dataset.sql
 \i deapp/de_variant_population_data.sql
 \i deapp/de_variant_population_info.sql
